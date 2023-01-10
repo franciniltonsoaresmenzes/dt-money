e4514f18eb4116bd65e4e31f9d8d2ec554eb713e
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2023-01-09 21:08:33 -0300

    criando componente Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "styled-components";
+import { Transactions } from "./pages/Transactions";
 import { GlobalStyles } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
@@ -7,7 +8,7 @@ export function App() {
   return (
     <ThemeProvider theme={defaultTheme} >
       <GlobalStyles />
-      <h1>Hello Word</h1>
+      <Transactions />
     </ThemeProvider>
   )
 }
