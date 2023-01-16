04f88282874d17b08cf8bcb5946424d3866c1108
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2023-01-15 21:07:03 -0300

    utilizando memo

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -5,6 +5,7 @@ import * as z from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { TransactionsContext } from '../../../../contexts/TransactionsContext'
 import { useContextSelector } from 'use-context-selector'
+import { memo } from 'react'
 
 const searchSchema = z.object({
   query: z.string(),
@@ -12,7 +13,7 @@ const searchSchema = z.object({
 
 type SearchFormInputs = z.infer<typeof searchSchema>
 
-export function SearchForm() {
+function SearchFormComponent() {
   const fetchTransactions = useContextSelector(
     TransactionsContext,
     (context) => context.fetchTransactions,
@@ -44,3 +45,5 @@ export function SearchForm() {
     </SearchFormContainer>
   )
 }
+
+export const SearchForm = memo(SearchFormComponent)
